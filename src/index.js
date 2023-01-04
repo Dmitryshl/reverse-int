dbe46b00a470d410def37b93e6f2951bba681299
Dmitry Shlyatsky <42865006+Dmitryshl@users.noreply.github.com>
2023-01-04 23:01:07 +0300

    init:solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0)
+    return Math.abs(n).toString().split('').reverse().join('');
+    else
+    return n.toString().split('').reverse().join('');
 }
